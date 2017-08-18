3f87f1151c8b5f72e2324526234a2d10b8188840
sdlyyxy <sdlyyxy@icloud.com>
2017-08-18 15:11:49 +0800

    修改demo && 增加README

---
+++ README.md
@@ -0,0 +1,5 @@
+# C# FTP操作类 
+
+感谢[http://blog.csdn.net/chinacsharper/article/details/9501773](http://blog.csdn.net/chinacsharper/article/details/9501773)。
+
+功能、用法很简单，直接看代码、demo即可。
--- cs_ftp/demo.cs
+++ cs_ftp/demo.cs
@@ -7,11 +7,6 @@ namespace cs_ftp
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //FtpWeb myFtp = new FtpWeb("123.206.93.234", "uftp", "ftpftp");
-            //foreach (var i in myFtp.GetFileList("*.*")){
-            //    Console.WriteLine(i);
-            //}
-            //myFtp.Download("/public/","ftp://123.206.93.234/123.xrx","123.xrx");
             HandyFtp myFtp = new HandyFtp("123.206.93.234", "uftp", "ftpftp");
             foreach (var i in myFtp.GetFileList()){
                 Console.WriteLine(i);
